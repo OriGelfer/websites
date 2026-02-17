8839308b4c8ae6d0013fd861ff92e086168bf254
OriGelfer <origs312@gmail.com>
2026-02-17 14:34:26 +0200

    מנהל וחיפוש

--- OriGelferWebsite/controller2.aspx
+++ OriGelferWebsite/controller2.aspx
@@ -3,5 +3,17 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" Runat="Server">
-    <h1>מנהל</h1>
+   
+         <form name="signupForm" method="post" runat="server">
+                    <label for="fname">:שם פרטי</label><br>
+   <input type="text" id="fname" name="fname" value=""><br>
+<label for="lname">:שם משפחה</label><br>
+<input type="text" id="lname" name="lname" value=""><br><br>
+              <input type="submit" value="Submit">
+             </form>
+
+     <center>
+    <h1>טבלת משתמשים</h1>
+    <%=st %>
+    </center>
 </asp:Content>
--- OriGelferWebsite/controller2.aspx.cs
+++ OriGelferWebsite/controller2.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,8 +8,55 @@ using System.Web.UI.WebControls;
 
 public partial class controller2 :  System.Web.UI.Page
 {
+    public string st = "";
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (Page.IsPostBack)
+        {
+            string fname = Request.Form["fname"];
+            string lname = Request.Form["lname"];
+
+            string sqlSelect = "SELECT * FROM tUsers WHERE " +
+             "fname = N'" + fname + "' AND " +
+             "lname = N'" + lname + "'";
+
+            DataTable dt = MyAdoHelper.ExecuteDataTable(sqlSelect);
+
+            if (dt.Rows.Count == 0)
+            {
+                st = "אין נתונים";
+            }
+            else
+            {
+                st += "<table border='1'>";
+                st += "<tr>";
+                st += "<th>שם פרטי</th>";
+                st += "<th>שם משפחה</th>";
+                st += "<th>אימייל</th>";
+                st += "<th>סיסמא</th>";
+                st += "<th>מספר טלפון</th>";
+                st += "<th>ענפי ספורט</th>";
+                st += "<th>?מסכים לעדכונים</th>";
+                st += "<th>הודעה</th>";
+                st += "<th>גיל</th>";
+                st += "</tr>";
+
+
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    st += "<tr>";
+                    for (int j = 0; j < dt.Columns.Count; j++)
+                    {
+                        st += "<td>";
+                        st += dt.Rows[i][j];
+                        st += "</td>";
+                    }
+
+                    st += "</tr>";
+                }
+                st += "</table>";
+            }
+        }
 
     }
 }
